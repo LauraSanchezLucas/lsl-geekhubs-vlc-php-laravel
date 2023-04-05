036709e0f59df53b9bba86ae44a2966eb287e66c
Laura <Lausnclu@hotmail.com>
2023-04-05 17:57:21 +0200

    feat: Migration games created with relation with user

--- discord/database/migrations/2023_04_05_150709_create_games_table.php
+++ discord/database/migrations/2023_04_05_150709_create_games_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable(false)->unique();
+            $table->string('platform');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
